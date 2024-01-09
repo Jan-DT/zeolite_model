d0790555a84c573251633200a380ac55d91114d7
Jan-DT <65350470+Jan-DT@users.noreply.github.com>
2024-01-09 10:37:11 +0100

    toevoegen derivative

--- main.py
+++ main.py
@@ -14,10 +14,12 @@ Adsorbed_H2O_0 = 0   # Amount of water in reactor at start.
 
 
 if __name__ == "__main__":
+    R = 8.314  # J/mol K
+    
     def plot_3d(T_values, p_values, q_f):
         T_values, p_values = np.meshgrid(T_values, p_values)
 
-        ax = fig.add_subplot(1, 2, 1, projection='3d')
+        ax = fig.add_subplot(1, 3, 1, projection='3d')
 
         # Plot the surface
         ax.plot_surface(T_values, p_values / 10**5, q_f(T_values, p_values))
@@ -28,12 +30,24 @@ if __name__ == "__main__":
 
 
     def plot_2d(T_values, p_values, q_f):
-        ax = fig.add_subplot(1, 2, 2)
+        ax = fig.add_subplot(1, 3, 2)
 
         for _T in T_values:
-            plt.plot(p_values / 10**5, q_f(_T, p_values), label=f"T = {_T} K")
+            plt.plot((p_values) / (R * _T), q_f(_T, p_values), label=f"T = {_T} K")
 
-        ax.set_xlabel("p (bar)")
+        ax.set_xlabel("C (mol/m^3)")
+        ax.set_ylabel("q (kg/kg)")
+
+        plt.legend()
+
+
+    def plot_2d_der(T_values, p_values, q_f):
+        ax = fig.add_subplot(1, 3, 3)
+
+        for _T in T_values:
+            plt.plot((p_values) / (R * _T), np.gradient(q_f(_T, p_values)), label=f"T = {_T} K")
+
+        ax.set_xlabel("C (mol/m^3)")
         ax.set_ylabel("q (kg/kg)")
 
         plt.legend()
@@ -43,7 +57,6 @@ if __name__ == "__main__":
 
     Q_MAX = 1.4  # mol/kg (uit zelfde paper)
     E_ADS = 54 * 10 ** 3  # J/mol (schatting)
-    R = 8.314  # J/mol K
     T0 = 298.15  # K
     M_CH4 = 16.04 * 10**-3  # kg/mol
 
@@ -53,7 +66,7 @@ if __name__ == "__main__":
     # uit paper: https://www.sciencedirect.com/science/article/abs/pii/S138718111400448X
     # B_A0 = 3.915 * 10 ** 5  # pa^-1
 
-    B_A0 = 100  #b_A0(dH0, dS0, T0, R)
+    B_A0 = 10e5  #b_A0(dH0, dS0, T0, R)
     print(B_A0)
 
     AMOUNT = 7
@@ -70,8 +83,9 @@ if __name__ == "__main__":
     def dq_dc(T_ADS, pA):
         return np.gradient(q_func(T_ADS, pA))
 
-    plot_2d(T_values, p_values, dq_dc)
-    plot_3d(T_values, p_values, dq_dc)
+    plot_2d(T_values, p_values, q_func)
+    plot_2d_der(T_values, p_values, q_func)
+    plot_3d(T_values, p_values, q_func)
 
     plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05)
 
